354ada97d5a3bec80a01e44e13693ec38e03b6b8
Your Name <you@example.com>
2024-05-31 20:47:50 -0400

    committing months worth of changes

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # wallpaper
 swww init
-swww img ~/.config/hypr/nebula.png
+swww img ~/.config/hypr/sand.png
 
 # laucher bar
 waybar -c ~/.config/hypr/waybar.conf -s ~/.config/hypr/waybar.style &
